8e5cc33f8a32fab6c630f3f937f9caf362f5f29d
Lirae Que Data <liraedata59@gmail.com>
2023-12-01 01:56:15 +0800

    Update views

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,30 +1,28 @@
-import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
+import "package:firebase_core/firebase_core.dart";
 import "package:frontend/routes.dart";
 import "package:frontend/firebase_options.dart";
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options : DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-   
-  runApp( const MyApp() );
+
+  runApp(const MyApp());
   return;
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key ? key }) : super(key : key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title : "PackagePal",
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      routes : routes
-    );
+        title: "PackagePal",
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/",
+        routes: routes);
   }
 }
