3ba6f2d9c8716f9b58577554f9a65ff18ee86c74
Lirae Que Data <liraedata59@gmail.com>
2023-12-01 00:59:56 +0800

    Add new routes

---
+++ frontend/lib/routes.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+Map<String, Widget Function(BuildContext)> routes = {
+  "/" : (context) {
+    return Container();
+  },
+
+  "/aut/login" : (context) {
+    return Container();
+  },
+
+  "/auth/register" :  (context) {
+    return Container();
+  },
+
+  "auth/forgot-password" : (context) {
+    return Container();
+  },
+
+  "/dashboard/home" : (context){
+    return Container();
+  },
+
+  "/dashboard/setting/change-pin" : (context) {
+    return Container();
+  }
+};
