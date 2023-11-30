53bdcb06d195d2b61af421d480d701d9231f8eee
Lirae Que Data <liraedata59@gmail.com>
2023-12-01 01:08:27 +0800

    Add views to routes

--- frontend/lib/routes.dart
+++ frontend/lib/routes.dart
@@ -1,24 +1,29 @@
 import "package:flutter/material.dart";
+import "package:frontend/views/Onboarding/index.dart";
+import "package:frontend/views/Dashboard/index.dart";
+import "package:frontend/views/ForgotPassword/index.dart";
+import "package:frontend/views/Login/index.dart";
+import "package:frontend/views/Register/index.dart";
 
 Map<String, Widget Function(BuildContext)> routes = {
   "/" : (context) {
-    return Container();
+    return const OnboardingView();
   },
 
   "/aut/login" : (context) {
-    return Container();
+    return const LoginView();
   },
 
   "/auth/register" :  (context) {
-    return Container();
+    return const RegisterView();
   },
 
   "auth/forgot-password" : (context) {
-    return Container();
+    return const ForgotPasswordView();
   },
 
-  "/dashboard/home" : (context){
-    return Container();
+  "/dashboard" : (context){
+    return const Dashboard();
   },
 
   "/dashboard/setting/change-pin" : (context) {
