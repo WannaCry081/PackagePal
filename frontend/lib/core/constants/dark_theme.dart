618a7681939aec54da7b516acc0079da9670d109
Lirae Que Data <liraedata59@gmail.com>
2023-12-01 14:08:07 +0800

    Add dark theme colors

--- frontend/lib/core/constants/dark_theme.dart
+++ frontend/lib/core/constants/dark_theme.dart
@@ -2,5 +2,12 @@ import "package:flutter/material.dart";
 
 ThemeData darkTheme = ThemeData(
   useMaterial3: true,
-  brightness: Brightness.dark
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme.dark(
+    primary: Color.fromARGB(255, 167, 222, 219),
+    secondary: Color.fromARGB(255, 160, 131, 203),
+    tertiary: Color.fromARGB(255, 215, 165, 131),
+    background: Color.fromARGB(255, 32, 38, 46),
+    onBackground: Colors.white
+  )
 );
