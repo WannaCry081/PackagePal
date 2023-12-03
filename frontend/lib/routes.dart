bf91ab001b89fdb63800b68d6acfb4659ff72249
Lirae Que Data <liraedata59@gmail.com>
2023-12-03 23:45:56 +0800

    Update routing key

--- frontend/lib/routes.dart
+++ frontend/lib/routes.dart
@@ -18,7 +18,7 @@ Map<String, Widget Function(BuildContext)> routes = {
     return const RegisterView();
   },
 
-  "auth/forgot-password" : (context) {
+  "/auth/forgot-password" : (context) {
     return const ForgotPasswordView();
   },
 
