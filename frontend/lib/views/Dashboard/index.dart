02f441fd43e01f0c5a67b6932ded514bdb5fb5f7
Lirae Que Data <liraedata59@gmail.com>
2023-12-05 13:13:29 +0800

    Update dashboard ui

--- frontend/lib/views/Dashboard/index.dart
+++ frontend/lib/views/Dashboard/index.dart
@@ -1,10 +1,26 @@
+import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import "package:frontend/viewmodels/auth_viewmodel.dart";
+import "package:provider/provider.dart";
+import "package:frontend/core/providers/user_provider.dart";
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({super.key});
+class DashboardView extends StatelessWidget {
+  const DashboardView({ Key ? key }) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final user = Provider.of<UserProvider>(context);
+
+    return Scaffold(
+      body : Center(
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children : [
+            Text("${FirebaseAuth.instance.currentUser?.displayName}"),
+            ElevatedButton(onPressed: () => AuthViewModel().signOutGoogle(), child: Text("Logout"))
+          ]
+        )
+      )
+    );
   }
 }
