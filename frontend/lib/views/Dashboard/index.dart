e1da0e4033356b230064c8cbeb5869c30c5d8e9e
Lirae Que Data <liraedata59@gmail.com>
2023-12-06 23:27:42 +0800

    Add dashboard user provider

--- frontend/lib/views/Dashboard/index.dart
+++ frontend/lib/views/Dashboard/index.dart
@@ -1,26 +1,23 @@
-import "package:firebase_auth/firebase_auth.dart";
+import "package:provider/provider.dart";
 import "package:flutter/material.dart";
 import "package:frontend/viewmodels/auth_viewmodel.dart";
-import "package:provider/provider.dart";
 import "package:frontend/core/providers/user_provider.dart";
 
 class DashboardView extends StatelessWidget {
-  const DashboardView({ Key ? key }) : super(key : key);
+  const DashboardView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserProvider>(context);
-
+    final userProvider = Provider.of<UserProvider>(context);
+    final data = userProvider.getUserData();
     return Scaffold(
-      body : Center(
-        child : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children : [
-            Text("${FirebaseAuth.instance.currentUser?.displayName}"),
-            ElevatedButton(onPressed: () => AuthViewModel().signOutGoogle(), child: Text("Logout"))
-          ]
-        )
-      )
-    );
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text("${data}"),
+      ElevatedButton(
+          onPressed: () => AuthViewModel().signOutGoogle(),
+          child: const Text("Logout"))
+    ])));
   }
 }
