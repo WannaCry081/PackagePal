c9acd57b3fcaf107016d03250f23095832c75564
shielamae02 <shiela.mlepon@gmail.com>
2023-12-07 00:28:05 +0800

    Add create order

--- frontend/lib/views/CreateOrder/index.dart
+++ frontend/lib/views/CreateOrder/index.dart
@@ -1,10 +1,40 @@
 import "package:flutter/material.dart";
+import "package:flutter_feather_icons/flutter_feather_icons.dart";
+import "package:frontend/core/constants/text_theme.dart";
 
 class CreateOrderView extends StatelessWidget {
   const CreateOrderView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: IconButton(
+                      icon: const Icon(FeatherIcons.arrowLeft),
+                       onPressed: () => Navigator.of(context).pop()
+                    ),
+                  ),
+                  titleText(
+                    "Create Order",
+                    titleSize: 20.0,
+                    titleWeight: FontWeight.bold,
+                    titleColor: Colors.grey[700]
+                  )
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              
+            ],
+          )
+        ),
+      )
+    );
   }
 }
