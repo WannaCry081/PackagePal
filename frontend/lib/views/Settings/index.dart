0c9c4162ae591dfc37979821d6cbf0862fa1b320
shielamae02 <shiela.mlepon@gmail.com>
2023-12-07 00:28:19 +0800

    Add settings

--- frontend/lib/views/Settings/index.dart
+++ frontend/lib/views/Settings/index.dart
@@ -1,10 +1,34 @@
 import "package:flutter/material.dart";
+import "package:flutter_feather_icons/flutter_feather_icons.dart";
+import "package:frontend/core/constants/text_theme.dart";
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    titleText(
+                      "Settings",
+                      titleSize: 26.0,
+                      titleColor: Colors.grey[800]
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        )
+      )
+    );
   }
 }
