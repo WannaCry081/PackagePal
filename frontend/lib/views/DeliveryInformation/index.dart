402b91d52ac165d8e0c54e0b8d884217171487f9
shielamae02 <shiela.mlepon@gmail.com>
2023-12-07 18:10:34 +0800

    Add edit delivery info

---
+++ frontend/lib/views/DeliveryInformation/index.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class EditDeliveryInfo extends StatefulWidget {
+  const EditDeliveryInfo({super.key});
+
+  @override
+  State<EditDeliveryInfo> createState() => _EditDeliveryInfoState();
+}
+
+class _EditDeliveryInfoState extends State<EditDeliveryInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
