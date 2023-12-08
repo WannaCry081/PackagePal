e359d5599e917f171089aa1d0268f6bed32f62e2
shielamae02 <shiela.mlepon@gmail.com>
2023-12-09 01:03:00 +0800

    Add fetch and add user credential functions

--- frontend/lib/core/providers/user_provider.dart
+++ frontend/lib/core/providers/user_provider.dart
@@ -1,6 +1,8 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
 import "package:frontend/models/user_model.dart";
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:frontend/viewmodels/database_viewmodel.dart';
 
 class UserProvider extends ChangeNotifier {
   Map<String, dynamic>? _user;
@@ -16,6 +18,23 @@ class UserProvider extends ChangeNotifier {
 
   void setUserData(UserModel data) {
     _user = data.toMap();
+    return;
+  }
+
+  Future<void> fetchUserCredential() async {
+    DocumentSnapshot<Map<String, dynamic>> docSnapshot = await 
+      DatabaseViewModel().getUserCredential();
+
+    notifyListeners();
+    return;
+  }
+
+  Future<void> addUserCredential(Map<String, dynamic> data) async {
+    await DatabaseViewModel().addUserCredential(
+      data
+    );
+
+    fetchUserCredential();
     return;
   }
 
