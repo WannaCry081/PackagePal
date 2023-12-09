0ca504ef87204925724dbdff13dedf72456fa93b
shielamae02 <shiela.mlepon@gmail.com>
2023-12-09 20:37:20 +0800

    Add functionality to create order

--- frontend/lib/views/CreateOrder/index.dart
+++ frontend/lib/views/CreateOrder/index.dart
@@ -1,9 +1,15 @@
 import "package:flutter/material.dart";
 import "package:flutter_feather_icons/flutter_feather_icons.dart";
 import "package:frontend/core/constants/text_theme.dart";
+import "package:frontend/core/providers/order_provider.dart";
+import "package:frontend/models/order_model.dart";
 import "package:frontend/widgets/CustomButton.dart";
 import "package:frontend/widgets/CustomFormField.dart";
 import "package:google_fonts/google_fonts.dart";
+import 'package:intl/intl.dart';
+import 'dart:math';
+
+import "package:provider/provider.dart";
 
 class CreateOrderView extends StatefulWidget {
   const CreateOrderView({super.key});
@@ -18,9 +24,10 @@ class _CreateOrderViewState extends State<CreateOrderView> {
   late TextEditingController _pin;
   late TextEditingController _weight;
   late TextEditingController _price;
-  late TextEditingController _driverName;
-  late TextEditingController _driverNumber;
+  late TextEditingController _deliveryName;
+  late TextEditingController _deliveryContact;
   late TextEditingController _status;
+  late TextEditingController _deliveryDate;
 
   @override
   void initState() {
@@ -29,9 +36,10 @@ class _CreateOrderViewState extends State<CreateOrderView> {
     _pin = TextEditingController(text: "");
     _weight = TextEditingController(text: "");
     _price = TextEditingController(text: "");
-    _driverName = TextEditingController(text: "");
-    _driverNumber = TextEditingController(text: "");
+    _deliveryName = TextEditingController(text: "");
+    _deliveryContact = TextEditingController(text: "");
     _status = TextEditingController(text: "On Process");
+    _deliveryDate = TextEditingController(text: "");
   }
 
   @override
@@ -41,15 +49,57 @@ class _CreateOrderViewState extends State<CreateOrderView> {
     _pin.dispose();
     _weight.dispose();
     _price.dispose();
-    _driverName.dispose();
-    _driverNumber.dispose();
+    _deliveryName.dispose();
+    _deliveryContact.dispose();
     _status.dispose();
+    _deliveryDate.dispose();
   }
 
+  void generateRandomPin() {
+    const digits = '0123456789';
+    final random = Random();
+
+    String pinCode = "";
+    for (int i = 0; i < 8; i++) { 
+      pinCode += digits[random.nextInt(digits.length)];
+    }
+
+    _pin.text = pinCode;
+  }
+
+  void clearController(){
+    _packageName.clear();
+    _pin.clear();
+    _weight.clear();
+    _price.clear();
+    _deliveryName.clear();
+    _deliveryContact.clear();
+    _status.clear();
+    _deliveryDate.clear();
+  }
+
+  Future<void> _createOrder(OrderProvider orderProvider) async {
+    await orderProvider.createOrder([
+      OrderModel(
+        name: _packageName.text.trim(),
+        pin: _pin.text.trim(),
+        weight: _weight.text.trim(),
+        price: _price.text.trim(),
+        status: _status.text.trim(),
+        deliveryName: _deliveryName.text.trim(),
+        deliveryContact: _deliveryContact.text.trim(),
+        deliveryDate: _deliveryDate.text.trim(),
+      ).toMap(),
+    ]);
+
+    clearController();
+  }
 
 
   @override
   Widget build(BuildContext context) {
+    final orderProvider = Provider.of<OrderProvider>(context);
+    
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -163,6 +213,51 @@ class _CreateOrderViewState extends State<CreateOrderView> {
                               )
                             ],
                           ),
+
+                          const SizedBox(height: 10),
+
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: TextField(
+                              controller: _deliveryDate,
+                              readOnly: true,
+                              decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Theme.of(context).colorScheme.primary.withAlpha(25),
+                              counterText: "",
+                              labelStyle: GoogleFonts.poppins(fontSize: 14, color: Colors.grey[600]),
+                              hintText: "Delivery Date",
+                              suffixIcon: Icon(FeatherIcons.calendar, color: Colors.grey[600]),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.grey.shade200)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.grey.shade200)),
+                              errorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.red.shade300)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Theme.of(context).colorScheme.primary))),
+                              onTap: () async {
+                                DateTime? pickedDate = await showDatePicker(
+                                  context: context, 
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(1950), 
+                                  lastDate: DateTime(2100));
+
+                                if(pickedDate != null){
+                                  String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+                                  setState(() {
+                                    _deliveryDate.text = formattedDate;
+                                  });
+                                }
+                              },
+                            
+                            ),
+                          )
                         ],
                       )
                     )
@@ -188,7 +283,7 @@ class _CreateOrderViewState extends State<CreateOrderView> {
                           Padding(
                              padding: const EdgeInsets.symmetric(horizontal:20.0),
                              child: titleText(
-                              "Setup 10-PIN Code",
+                              "Setup 8-PIN Code",
                               titleSize: 20.0,
                               titleColor: Theme.of(context).colorScheme.secondary,
                               titleWeight: FontWeight.bold)),
@@ -202,22 +297,27 @@ class _CreateOrderViewState extends State<CreateOrderView> {
                                   formData: _pin,
                                   formLabelText: "PIN Code"),
                               ),
-                              Container(
-                                margin: const EdgeInsets.only(right: 20),
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                height: 55,
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Center(
-                                  child: bodyText(
-                                    "Auto Generate",
-                                    bodySize: 14.0,
-                                    bodyColor: Colors.white,
-                                    bodyWeight: FontWeight.w600
+                              GestureDetector(
+                                onTap: () => {
+                                  generateRandomPin()
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 20),
+                                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                                  height: 55,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).colorScheme.primary,
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                )
+                                  child: Center(
+                                    child: bodyText(
+                                      "Auto Generate",
+                                      bodySize: 14.0,
+                                      bodyColor: Colors.white,
+                                      bodyWeight: FontWeight.w600
+                                    ),
+                                  )
+                                ),
                               )
                             ],
                           ),
@@ -254,13 +354,13 @@ class _CreateOrderViewState extends State<CreateOrderView> {
                           const SizedBox(height: 20),
             
                           CustomFormField(
-                            formData: _driverName,
+                            formData: _deliveryName,
                             formLabelText: "Name" ),
                           
                           const SizedBox(height: 10),
             
                           CustomFormField(
-                            formData: _driverNumber,
+                            formData: _deliveryContact,
                             formLabelText: "Contact Number" ),                  
                         ],
                       )
@@ -274,7 +374,9 @@ class _CreateOrderViewState extends State<CreateOrderView> {
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: CustomButton(
                     btnColor: Theme.of(context).colorScheme.primary,
-                    btnOnTap: () {},
+                    btnOnTap: () {
+                      _createOrder(orderProvider);
+                    },
                     btnChild: Text("Submit Order",
                         style: GoogleFonts.lato(
                             fontSize: 16,
@@ -290,4 +392,5 @@ class _CreateOrderViewState extends State<CreateOrderView> {
       )
     );
   }
+
 }
