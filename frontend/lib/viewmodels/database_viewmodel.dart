5028d251250ecdea98a1daa4bc61d82c00194735
shielamae02 <shiela.mlepon@gmail.com>
2023-12-09 20:36:46 +0800

    Add createOrder and getOrders to database view model

--- frontend/lib/viewmodels/database_viewmodel.dart
+++ frontend/lib/viewmodels/database_viewmodel.dart
@@ -7,7 +7,7 @@ class DatabaseViewModel {
   final String _userUid = AuthViewModel().getUserUID;
   final String _email = AuthViewModel().getUserEmail;
 
-  //User 
+  // User 
   Future<DocumentSnapshot<Map<String, dynamic>>> getUserCredential() async {
     final collection = _firestore.collection(_userUid).doc("$_userUid+$_email");
     return await collection.get();
@@ -23,5 +23,45 @@ class DatabaseViewModel {
     });
     return;
   }
+
+
+  // Orders
+  Future<void> createOrder(List<dynamic> data) async {
+  final orderCollection = _firestore.collection(_userUid).doc("$_userUid+Orders");
+
+  DocumentSnapshot<Map<String, dynamic>> documentSnapshot = await orderCollection.get();
+  Map<String, dynamic> currentData = documentSnapshot.data() ?? {};
+
+  List<dynamic> ordersList = currentData["Orders"] ?? [];
+  
+  ordersList.addAll(data);
+
+  await orderCollection.set({
+    "Orders": ordersList,
+  });
+
+  return;
+}
+
+
+  Stream<List<dynamic>> getOrders() {
+  final orderCollection = _firestore.collection(_userUid).doc("$_userUid+Orders");
+
+  return orderCollection.snapshots().map((snapshot) {
+    if (snapshot.exists) {
+      final data = snapshot.data() as Map<String, dynamic>;
+
+      if (data['Orders'] != null) {
+        final List<dynamic> orderList = data['Orders'];
+        return orderList;
+      } else {
+        return [];
+      }
+    } else {
+      return [];
+    }
+  });
+}
+
 
 }
