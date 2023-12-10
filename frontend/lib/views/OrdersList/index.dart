3bf72f328f3991208c5314b8d91aadfed91bc0a3
shielamae02 <shiela.mlepon@gmail.com>
2023-12-11 00:05:33 +0800

    Add streambuilder to orderslist

--- frontend/lib/views/OrdersList/index.dart
+++ frontend/lib/views/OrdersList/index.dart
@@ -1,12 +1,22 @@
 import "package:flutter/material.dart";
 import "package:frontend/core/constants/text_theme.dart";
+import "package:frontend/viewmodels/database_viewmodel.dart";
 import "package:frontend/views/OrderView/index.dart";
 import "package:frontend/widgets/OrderCard.dart";
 import "package:google_fonts/google_fonts.dart";
 
-class OrdersListView extends StatelessWidget {
+class OrdersListView extends StatefulWidget {
   const OrdersListView({super.key});
 
+  @override
+  State<OrdersListView> createState() => _OrdersListViewState();
+}
+
+class _OrdersListViewState extends State<OrdersListView> {
+
+  final db = DatabaseViewModel();
+  List<dynamic> orders= [];
+  
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -42,54 +52,103 @@ class OrdersListView extends StatelessWidget {
                 ),
               ]
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  Builder(
-                    builder: (context) => ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical:4.0, horizontal: 16.0),
-                          child: OrderCard(
-                            status: "On Process",
-                            orderCardOpenContents: () => {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) => const OrderView())
-                              )
-                            },
-                          ),
-                        );
-                      },
-                    )
+
+
+            StreamBuilder<Object>(
+              stream: db.getOrders(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      child: const CircularProgressIndicator(),
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: bodyText('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data == null) {
+                  return Center(child: bodyText('No data available'));
+                }
+
+                
+                List<dynamic> orders = snapshot.data as List<dynamic>;
+              
+                List<Map<String, dynamic>> onProcessOrders = orders.cast<Map<String, dynamic>>().toList();
+                List<Map<String, dynamic>> completedOrders = orders.cast<Map<String, dynamic>>().toList();
+
+                onProcessOrders = onProcessOrders
+                    .where((order) => order['status'] == 'On Process')
+                    .toList();
+                completedOrders = completedOrders
+                    .where((order) => order['status'] == 'Completed')
+                    .toList();
+                
+
+                return Expanded(
+                  child: TabBarView(
+                    children: [
+                      Builder(
+                        builder: (context) => ListView.builder(
+                          itemCount: onProcessOrders.length,
+                          itemBuilder: (context, index) {
+                            final order = onProcessOrders[index];
+
+                            final packageName = order['name'] as String;
+                            final status = order['status'] as String;
+                            final deliveryDate = order['deliveryDate'] as String;
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                              child: OrderCard(
+                                packageName: packageName,
+                                status: status,
+                                deliveryDate: deliveryDate,
+                                orderCardOpenContents: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(builder: (context) => OrderView(orderData: order, index: index)),
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Builder(
+                        builder: (context) => ListView.builder(
+                          itemCount: completedOrders.length,
+                          itemBuilder: (context, index) {
+                            final order = completedOrders[index];
+
+                            final packageName = order['name'] as String;
+                            final status = order['status'] as String;
+                            final deliveryDate = order['deliveryDate'] as String;
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                              child: OrderCard(
+                                packageName: packageName,
+                                status: status,
+                                deliveryDate: deliveryDate,
+                                orderCardOpenContents: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(builder: (context) => OrderView(orderData: order, index: index)),
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  Builder(
-                    builder: (context) => ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical:4.0, horizontal: 16.0),
-                          child: OrderCard(
-                            status: "Completed",
-                            orderCardOpenContents: () => {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => const OrderView()
-                                )
-                              )
-                            },
-                          ),
-                        );
-                      },
-                    )
-                  ),
-                ]
-              ),
+                );
+              },
             )
+
           ]
         )
       ),
     );
   }
-  
 }
