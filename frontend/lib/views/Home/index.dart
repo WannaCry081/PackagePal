d278a30a802e6b54bc5f673bf2d13bb384fffde3
shielamae02 <shiela.mlepon@gmail.com>
2023-12-14 07:29:33 +0800

    Update home view

--- frontend/lib/views/Home/index.dart
+++ frontend/lib/views/Home/index.dart
@@ -3,22 +3,46 @@ import "package:flutter_feather_icons/flutter_feather_icons.dart";
 import "package:flutter_svg/flutter_svg.dart";
 import "package:frontend/core/constants/text_theme.dart";
 import "package:frontend/core/providers/user_provider.dart";
+import "package:frontend/core/utils/FormValidator.dart";
 import "package:frontend/viewmodels/auth_viewmodel.dart";
 import "package:frontend/viewmodels/database_viewmodel.dart";
 import "package:frontend/views/OrderView/index.dart";
+import "package:frontend/widgets/CustomButton.dart";
+import "package:frontend/widgets/CustomDialogBox.dart";
+import "package:frontend/widgets/CustomFormField.dart";
 import "package:frontend/widgets/OrderCard.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:provider/provider.dart";
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeView> createState() => _HomeViewState();
+}
 
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
     final db = DatabaseViewModel();
     final userProvider = Provider.of<UserProvider>(context);
     final data = userProvider.getUserData();
+
+    final GlobalKey<FormState> _form = GlobalKey<FormState>();
+    late TextEditingController _code;
+    
+
+    @override
+    void initState() {
+      _code =  TextEditingController(text: "");
+    }
+
+    @override
+    void dispose(){
+      super.dispose();
+      _code.dispose();
+    }
+
 
     return Container(
       color: const Color.fromARGB(255, 13, 21, 23),
@@ -33,10 +57,6 @@ class HomeView extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    bodyText(
-                      "",
-                    bodySize: 16.0,
-                    bodyColor: Colors.white),
                     const Spacer(),
                     Container(
                       margin: const EdgeInsets.only(right: 8),
@@ -107,47 +127,59 @@ class HomeView extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             titleText(
-                              "Check Your Package",
+                              "Start adding orders now!",
                               titleSize: 24.0,
                               titleWeight: FontWeight.bold,
                             ),
-                            const SizedBox(height: 4),
-                            bodyText(
-                              "Enter the name of the package you want to view",
-                              bodySize: 12.0,
-                              bodyColor: Colors.grey[700],
-                              bodyAlignment: TextAlign.center,
+                            const SizedBox(height: 5),
+                             bodyText(
+                              "Receive packages effortlessly and hassle-free!",
                             ),
-                            const SizedBox(height: 10),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10.0),
-                              child: TextField(
-                                style: GoogleFonts.poppins(fontSize : 14.0),
-                                decoration: InputDecoration(
-                                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(25.0),
-                                    borderSide: BorderSide.none
-                                  ),
-                                    hintStyle: GoogleFonts.poppins(
-                                      fontSize : 14.0,
+                           
+                            const SizedBox(height: 20),
+                            Form(
+                              key: _form,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: CustomButton(
+                                      btnColor: Theme.of(context).colorScheme.primary,
+                                      btnOnTap: () {
+                                          showDialog(context: context, 
+                                          builder: (context){
+                                            return CustomDialogBox(
+                                              dialogTitle: "Connect to Firebase", 
+                                                dialogContent: CustomFormField(
+                                                  formData: _code,
+                                                  formLabelText: "Unique Code",
+                                                  formValidator: (value) => FormValidator().validateInput(value, "Code", 2, 30),
+                                                ),
+                                                dialogOption1: "Cancel", 
+                                                dialogOption2: "Connect",
+                                                dialogOption1OnTap: () => Navigator.of(context).pop(),
+                                                dialogOption2OnTap: (){},);
+                                              });
+                                          },
+                                          btnChild: Text("Connect",
+                                              style: GoogleFonts.lato(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600,
+                                                  color: Theme.of(context).colorScheme.background)),
                                     ),
-                                    hintText: 'Enter the package name',
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                    suffixIcon: Container(
-                                      margin: const EdgeInsets.all(6.0),
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Theme.of(context).colorScheme.secondary
-                                      ),
-                                      child: const Icon(
-                                        FeatherIcons.search,
-                                        color: Colors.white,
-                                        size: 18.0
-                                      ),
-                                    )
-                                ),
+                                  ),
+                                  const SizedBox(width: 5,),
+                                  Expanded(
+                                    child: CustomButton(
+                                      btnColor: Theme.of(context).colorScheme.primary,
+                                      btnOnTap: () {},
+                                          btnChild: Text("Open Container",
+                                              style: GoogleFonts.lato(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600,
+                                                  color: Theme.of(context).colorScheme.background)),
+                                    ),
+                                  ),
+                                ],
                               ),
                             )
                           ],
@@ -158,11 +190,12 @@ class HomeView extends StatelessWidget {
                     StreamBuilder(
                       stream: db.getOrdersOnToday(), 
                       builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else if (snapshot.hasError) {
+                      //   if (snapshot.connectionState == ConnectionState.waiting) {
+                      //   return const Center(
+                      //     child: CircularProgressIndicator(),
+                      //   );
+                      // } 
+                      if (snapshot.hasError) {
                         return Center(
                           child: Text('Error: ${snapshot.error}'),
                         );
@@ -173,11 +206,21 @@ class HomeView extends StatelessWidget {
                       }
                       
                        List<Map<String, dynamic>> orders = snapshot.data!.cast<Map<String, dynamic>>();
+                            print(orders.length);
+
+                        if(orders.isEmpty){
+                          return Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 25.0),
+                              child: bodyText(
+                                "No orders for today"),
+                            ),);
+                        }
 
                        return SizedBox(
                         height: 80,
                          child: ListView.builder(
-                          itemCount: 1,
+                          itemCount: orders.length,
                           scrollDirection: Axis.horizontal,
                           itemBuilder: (context, index){
                             final order = orders[index];
@@ -186,7 +229,7 @@ class HomeView extends StatelessWidget {
                             final status = order['status'] as String;
                             final deliveryDate = order['deliveryDate'] as String;
                          
-                            return Container(
+                            return SizedBox(
                               width: MediaQuery.of(context).size.width - 40,
                               child: OrderCard(
                                 packageName: packageName, 
