80d7d3d2f20d1318822173fdc201a3cb0be5bf96
shielamae02 <shiela.mlepon@gmail.com>
2024-02-26 22:04:44 +0800

    Update home view

--- frontend/lib/views/Home/index.dart
+++ frontend/lib/views/Home/index.dart
@@ -4,13 +4,10 @@ import "package:flutter_svg/flutter_svg.dart";
 import "package:frontend/core/constants/text_theme.dart";
 import "package:frontend/core/providers/user_provider.dart";
 import "package:frontend/core/utils/FormValidator.dart";
-import "package:frontend/viewmodels/auth_viewmodel.dart";
 import "package:frontend/viewmodels/database_viewmodel.dart";
-import "package:frontend/views/OrderView/index.dart";
 import "package:frontend/widgets/CustomButton.dart";
 import "package:frontend/widgets/CustomDialogBox.dart";
 import "package:frontend/widgets/CustomFormField.dart";
-import "package:frontend/widgets/OrderCard.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:provider/provider.dart";
 
@@ -22,27 +19,41 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  late TextEditingController _code;
+
+  
+  final db = DatabaseViewModel();
+  final realtimeDb = RealtimeDatabaseViewModel();
+  late String boxId = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBoxId();
+    _code =  TextEditingController(text: "");
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _code.dispose();
+  }
+
+   Future<void> fetchBoxId() async {
+    String id = await db.getBoxId();
+    setState(() {
+      boxId = id;
+    });
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     final db = DatabaseViewModel();
     final userProvider = Provider.of<UserProvider>(context);
     final data = userProvider.getUserData();
-
-    final GlobalKey<FormState> _form = GlobalKey<FormState>();
-    late TextEditingController _code;
-    
-
-    @override
-    void initState() {
-      _code =  TextEditingController(text: "");
-    }
-
-    @override
-    void dispose(){
-      super.dispose();
-      _code.dispose();
-    }
-
 
     return Container(
       color: const Color.fromARGB(255, 13, 21, 23),
@@ -58,21 +69,21 @@ class _HomeViewState extends State<HomeView> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     const Spacer(),
-                    Container(
-                      margin: const EdgeInsets.only(right: 8),
-                      height: 40,
-                      width: 40,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle
-                      ),
-                      child: Icon(
-                        FeatherIcons.moon,
-                        color: Theme.of(context).colorScheme.secondary,
-                        size: 20.0,
-                        weight: 5.0,
-                      )
-                    ),
+                    // Container(
+                    //   margin: const EdgeInsets.only(right: 8),
+                    //   height: 40,
+                    //   width: 40,
+                    //   decoration: const BoxDecoration(
+                    //     color: Colors.white,
+                    //     shape: BoxShape.circle
+                    //   ),
+                    //   child: Icon(
+                    //     FeatherIcons.moon,
+                    //     color: Theme.of(context).colorScheme.secondary,
+                    //     size: 20.0,
+                    //     weight: 5.0,
+                    //   )
+                    // ),
 
                     CircleAvatar(
                       backgroundImage: NetworkImage(data?['photoUrl'] ?? ""),
@@ -115,13 +126,12 @@ class _HomeViewState extends State<HomeView> {
                     ),
                     Container(
                       width: MediaQuery.of(context).size.width,
-                      height: 165,
                       decoration: BoxDecoration(
                         color: const Color.fromARGB(255, 246, 236, 226),
                         borderRadius: BorderRadius.circular(10.0)
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.all(10.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -139,46 +149,54 @@ class _HomeViewState extends State<HomeView> {
                             const SizedBox(height: 20),
                             Form(
                               key: _form,
-                              child: Row(
+                              child: Column(
                                 children: [
-                                  Expanded(
-                                    child: CustomButton(
-                                      btnColor: Theme.of(context).colorScheme.primary,
-                                      btnOnTap: () {
-                                          showDialog(context: context, 
-                                          builder: (context){
-                                            return CustomDialogBox(
-                                              dialogTitle: "Connect to Firebase", 
-                                                dialogContent: CustomFormField(
-                                                  formData: _code,
-                                                  formLabelText: "Unique Code",
-                                                  formValidator: (value) => FormValidator().validateInput(value, "Code", 2, 30),
-                                                ),
-                                                dialogOption1: "Cancel", 
-                                                dialogOption2: "Connect",
-                                                dialogOption1OnTap: () => Navigator.of(context).pop(),
-                                                dialogOption2OnTap: (){},);
-                                              });
-                                          },
-                                          btnChild: Text("Connect",
-                                              style: GoogleFonts.lato(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Theme.of(context).colorScheme.background)),
-                                    ),
+                                  CustomButton(
+                                    btnColor: Theme.of(context).colorScheme.primary,
+                                    btnOnTap: () async {
+                                      bool currentStatus = await realtimeDb.getDoorStatus(boxId);
+
+                                      await realtimeDb.updateDoorStatus(!currentStatus);
+        
+                                    },
+                                        btnChild: Text("Open Container",
+                                            style: GoogleFonts.lato(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600,
+                                                color: Theme.of(context).colorScheme.background)),
                                   ),
-                                  const SizedBox(width: 5,),
-                                  Expanded(
-                                    child: CustomButton(
-                                      btnColor: Theme.of(context).colorScheme.primary,
-                                      btnOnTap: () {},
-                                          btnChild: Text("Open Container",
-                                              style: GoogleFonts.lato(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Theme.of(context).colorScheme.background)),
-                                    ),
-                                  ),
+                                  const SizedBox(height: 10),
+                                  CustomButton(
+                                    btnColor: Theme.of(context).colorScheme.primary,
+                                    btnOnTap: () {
+                                      showDialog( context: context, 
+                                      builder: (context){
+                                        return CustomDialogBox(
+                                            dialogTitle: "Connect to my Container",
+                                            dialogSubtitle: "Enter the ID generated by the QR code.", 
+                                            dialogContent: CustomFormField(
+                                              formData: _code,
+                                              formLabelText: "Unique Code",
+                                              formValidator: (value) => FormValidator().validateInput(value, "Code", 2, 30),
+                                              horizontalPadding: 0,
+                                            ),
+                                            dialogOption1: "Cancel", 
+                                            dialogOption2: "Connect",
+                                            dialogOption1OnTap: () => Navigator.of(context).pop(),
+                                            dialogOption2OnTap: () async {
+                                             if(_form.currentState!.validate()){
+                                              db.addUniqueCode(_code.text.trim());
+                                              Navigator.of(context).pop();
+                                             } 
+                                            });
+                                          });
+                                      },
+                                    btnChild: Text("Connect to my Container",
+                                        style: GoogleFonts.lato(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600,
+                                            color: Theme.of(context).colorScheme.background)),
+                                  )
                                 ],
                               ),
                             )
@@ -186,69 +204,32 @@ class _HomeViewState extends State<HomeView> {
                         ),
                       )
                     ),
-                    const SizedBox(height: 10.0),
-                    StreamBuilder(
-                      stream: db.getOrdersOnToday(), 
-                      builder: (context, snapshot) {
-                      //   if (snapshot.connectionState == ConnectionState.waiting) {
-                      //   return const Center(
-                      //     child: CircularProgressIndicator(),
-                      //   );
-                      // } 
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text('Error: ${snapshot.error}'),
-                        );
-                      } else if (!snapshot.hasData || snapshot.data == null) {
-                        return const Center(
-                          child: Text('No data available'),
-                        );
-                      }
-                      
-                       List<Map<String, dynamic>> orders = snapshot.data!.cast<Map<String, dynamic>>();
-                            print(orders.length);
-
-                        if(orders.isEmpty){
-                          return Center(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 25.0),
-                              child: bodyText(
-                                "No orders for today"),
-                            ),);
-                        }
-
-                       return SizedBox(
-                        height: 80,
-                         child: ListView.builder(
-                          itemCount: orders.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index){
-                            final order = orders[index];
-                         
-                            final packageName = order['name'] as String;
-                            final status = order['status'] as String;
-                            final deliveryDate = order['deliveryDate'] as String;
-                         
-                            return SizedBox(
-                              width: MediaQuery.of(context).size.width - 40,
-                              child: OrderCard(
-                                packageName: packageName, 
-                                status: status,
-                                deliveryDate: deliveryDate,
-                                orderCardOpenContents: () => {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => OrderView(
-                                        orderData: order, index: index)
-                                    )
-                                  )
-                                },
-                              ),
-                            );
-                          }),
-                       );
-                      }
-                    )
+                    // CustomButton(
+                    //   btnColor: Theme.of(context).colorScheme.primary,
+                    //   btnOnTap: () {
+                    //     showDialog( context: context, 
+                    //     builder: (context){
+                    //       return CustomDialogBox(
+                    //           dialogTitle: "Connect to my Container",
+                    //           dialogSubtitle: "Enter the ID generated by the QR code.", 
+                    //           dialogContent: CustomFormField(
+                    //             formData: _code,
+                    //             formLabelText: "Unique Code",
+                    //             formValidator: (value) => FormValidator().validateInput(value, "Code", 2, 30),
+                    //             horizontalPadding: 0,
+                    //           ),
+                    //           dialogOption1: "Cancel", 
+                    //           dialogOption2: "Connect",
+                    //           dialogOption1OnTap: () => Navigator.of(context).pop(),
+                    //           dialogOption2OnTap: (){},);
+                    //         });
+                    //     },
+                    //   btnChild: Text("Connect to my Container",
+                    //       style: GoogleFonts.lato(
+                    //           fontSize: 16,
+                    //           fontWeight: FontWeight.w600,
+                    //           color: Theme.of(context).colorScheme.background)),
+                    // )
                   ] 
                 )
               )
